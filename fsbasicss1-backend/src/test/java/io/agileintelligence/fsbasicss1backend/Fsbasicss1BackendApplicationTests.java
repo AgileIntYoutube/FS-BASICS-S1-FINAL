004bdd76676095957f145aea4f5166439eefeecc
AgileIntelligence <io.agileintelligence@gmail.com>
2019-11-20 12:20:03 -0500

    EP01-BOOTSTRAP-PROJECT

---
+++ fsbasicss1-backend/src/test/java/io/agileintelligence/fsbasicss1backend/Fsbasicss1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.fsbasicss1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsbasicss1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
