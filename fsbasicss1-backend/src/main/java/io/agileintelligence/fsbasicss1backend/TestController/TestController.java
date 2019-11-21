6397bab4c4097fd43bdb8aa6219c4a6b8283633d
AgileIntelligence <io.agileintelligence@gmail.com>
2019-11-21 12:17:40 -0500

    EP04-QueryParam-PathVar

--- fsbasicss1-backend/src/main/java/io/agileintelligence/fsbasicss1backend/TestController/TestController.java
+++ fsbasicss1-backend/src/main/java/io/agileintelligence/fsbasicss1backend/TestController/TestController.java
@@ -1,8 +1,6 @@
 package io.agileintelligence.fsbasicss1backend.TestController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -12,4 +10,21 @@ public class TestController {
     public String testMethod(){
         return "This is a sample response";
     }
+
+    @GetMapping("/queryp")
+    public String queryParam(@RequestParam(value="carlos")String reqParam){
+        return reqParam;
+    }
+
+    @GetMapping("/{pv}/pathv")
+    public String pathVar(@PathVariable String pv){
+        return pv;
+    }
+
+    @GetMapping("/pathv/{pv}")
+    public String pathVar2(@PathVariable String pv){
+        return pv;
+    }
+
+
 }
